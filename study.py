16377563c483b919ffcc1d17d4dab56dc5418d7a
Filipe Silva <filipe.seixas@protonmail.com>
2021-07-01 11:29:07 +0100

    study.py added

---
+++ study.py
@@ -0,0 +1,11 @@
+# Create a list 1-5 and iterate through it
+# Print the 3rd number
+# Print list backwards
+# Create a dictionary with key value firstname and lastname
+# Add a key to it
+# Print value of lastname
+# Create a class called student, initialise the class and create an object of the class
+# Use super() in a class
+# Do an if statement
+# Do a while and for loop
+
