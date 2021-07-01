a9c67a3015b522c0cf0e00e37c2c9cc2084cb64b
Filipe Silva <filipe.seixas@protonmail.com>
2021-07-01 10:32:39 +0100

    task3 and 4 added

--- tasks.py
+++ tasks.py
@@ -1,13 +1,21 @@
-# Declare a function called username that takes one argument as a string and return the name
+# 1. Declare a function called username that takes one argument as a string and return the name
 # def username(name1):
 #     return name1
 #
 #
 # print(username("Filipe"))
 
-# Declare a list with number 1 - 5. Iterate through the list and display the list
+# 2. Declare a list with number 1 - 5. Iterate through the list and display the list
 
 list_1 = [1, 2, 3, 4, 5]
 for x in list_1:
     print(x)
 
+# 3. AND - && & == Which one returns a bool value?
+# ==
+
+# 4. What is the difference between a list and a tuple?
+# A list is mutable, and is defined with []
+# A tuple is immutable, and is defined with ()
+
+# 5.
