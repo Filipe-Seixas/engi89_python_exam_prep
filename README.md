e69bf416d7cc07fd502ed9562e2c75e9c857982c
Filipe Silva <filipe.seixas@protonmail.com>
2021-07-01 10:22:46 +0100

    readme added and tasks.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python Exam Preparation
+
---
+++ tasks.py
@@ -0,0 +1,6 @@
+# Declare a function called username that takes one argument as a string and return the name
+def username(name1):
+    return name1
+
+
+print(username("Filipe"))
