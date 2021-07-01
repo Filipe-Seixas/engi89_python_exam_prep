2741f901c190258f5eeab06f70c4cb664fb4a1c5
Filipe Silva <filipe.seixas@protonmail.com>
2021-07-01 10:25:17 +0100

    task2 added

--- tasks.py
+++ tasks.py
@@ -1,6 +1,13 @@
 # Declare a function called username that takes one argument as a string and return the name
-def username(name1):
-    return name1
+# def username(name1):
+#     return name1
+#
+#
+# print(username("Filipe"))
 
+# Declare a list with number 1 - 5. Iterate through the list and display the list
 
-print(username("Filipe"))
+list_1 = [1, 2, 3, 4, 5]
+for x in list_1:
+    print(x)
+
